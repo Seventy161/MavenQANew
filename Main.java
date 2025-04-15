978dfeff5e433d345e733973a10e8b75cb87bb30
Seventy161 <Seventy161@gmail.com>
2025-04-15 07:32:22 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BonusService service = new BonusService();
-        int bonus = service.calcBonus(amount: 5_000, isRegistered: true);
-        System.out.println(bonus);
-    }
-}
